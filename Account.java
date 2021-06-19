6c24ed6a5edf7431f4099a0ef65f31646e515467
JustSomeoneElse01 <86142999+JustSomeoneElse01@users.noreply.github.com>
2021-06-19 02:56:18 -0600

    lmao

---
+++ Account.java
@@ -0,0 +1,11 @@
+public class Account {
+
+  public static void main(String[] args) {
+    
+    String name = "JustSomeoneElse01"
+  
+    System.out.println("Hello I'm " + name);
+    
+  }
+  
+}
